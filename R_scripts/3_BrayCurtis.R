6558d162c258bd48d5d543aac7a981bdcdbc067a
Adelaide Roguet <33537775+AnehEf@users.noreply.github.com>
2019-09-07 17:24:25 -0500

    Update 3_BrayCurtis.R
    Export files with(out) capitalized letters

--- R_scripts/3_BrayCurtis.R
+++ R_scripts/3_BrayCurtis.R
@@ -105,8 +105,12 @@ for (x in 1:length(animal)) {
   
 }
 
+  
+BacterialGroup_o<-ifelse(BacterialGroup == "bacteroidales","Bacteroidales", "Clostridiales")
+
 braycurtis.final<-as.data.frame(braycurtis)
 names(braycurtis.final)<-animal
 write.table(braycurtis.final, paste0(output_directory, '_BrayCurtis_', BacterialGroup , '.txt'), sep="\t", quote=FALSE, col.names = TRUE, row.names = TRUE)
+write.table(braycurtis.final, paste0(output_directory, '_BrayCurtis_', BacterialGroup_o , '.txt'), sep="\t", quote=FALSE, col.names = TRUE, row.names = TRUE)
 
 }
