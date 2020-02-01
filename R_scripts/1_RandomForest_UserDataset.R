7031fe05f70dec923490f2ba138421c93d2d2e33
Adelaide Roguet <33537775+AnehEf@users.noreply.github.com>
2020-02-01 01:17:30 -0600

    Update 1_RandomForest_UserDataset.R

--- R_scripts/1_RandomForest_UserDataset.R
+++ R_scripts/1_RandomForest_UserDataset.R
@@ -27,7 +27,6 @@ bacterial_groups<-as.character(read.table(paste0("data/data_", region, "/bacteri
 pre.user<-read.table(User.Filename, h=T, sep="\t", row.names = 1)
 pre.user.1<-t(pre.user)
 pred.withzero.prob<-matrix(data = NA, nrow = nrow(pre.user.1),  ncol = length(animal)); row.names(pred.withzero.prob)<-row.names(pre.user.1)
-pred.withzero.response<-matrix(data = NA, nrow = nrow(pre.user.1),  ncol = length(animal)); row.names(pred.withzero.response)<-row.names(pre.user.1)
 pred.names<-vector(length=length(animal))
 
 for (y in 1:length(bacterial_groups)) {
@@ -65,20 +64,17 @@ for (y in 1:length(bacterial_groups)) {
         prediction.p.100<-prediction.p[, as.character(Animal)]*100
         prediction.p.100[which(apply(data.user, 1, sum)==0)]<-0
         pred.withzero.prob[, c(x)]<-prediction.p.100
+        if(region=='v6' && BacterialGroup=='bacteroidales' && Animal=='dog'){pred.withzero.prob[, c(x)]<-111}
+        if(region=='v6' && BacterialGroup=='bacteroidales' && Animal=='deer'){pred.withzero.prob[, c(x)]<-111}
+        if(region=='v4' && BacterialGroup=='bacteroidales' && Animal=='ruminant'){pred.withzero.prob[, c(x)]<-111}
         pred.names[x]<-as.character(Animal)
         #prediction response
         pred.withzero.prob.dataframe<-as.data.frame(pred.withzero.prob[, c(x)])
-        pred.withzero.response[,x]<-apply(pred.withzero.prob.dataframe, 2, function(y) ifelse(y > 50, paste(Animal, "(a)", sep=""), ifelse(y > 45, paste(Animal, "(b)", sep=""), ifelse(y > 40, paste(Animal, "(c)", sep=""), paste("-")))))
     }
 
     pred.withzero.prob.final<-rbind(pred.names, pred.withzero.prob); rownames(pred.withzero.prob.final)[1]<-" "
 
     write.table(pred.withzero.prob.final, paste0(Output.Directory, '_', BacterialGroup ,'_predprob.txt'), sep="\t", quote=FALSE, col.names = FALSE)
 
-    pred.withzero.response.simple<-as.character(interaction(as.data.frame(pred.withzero.response),sep="/"))
-    pred.withzero.response.simple.1<-as.data.frame(gsub("-/", "", pred.withzero.response.simple))
-    pred.withzero.response.simple.final<-as.data.frame(lapply(pred.withzero.response.simple.1, gsub, pattern = "/-", replacement = "", fixed = FALSE))
-    row.names(pred.withzero.response.simple.final)<-rownames(pred.withzero.prob)
-    write.table(pred.withzero.response.simple.final, paste0(Output.Directory, '_', BacterialGroup , '_predresponse_simple.txt'), sep="\t", quote=FALSE, col.names = FALSE, row.names = TRUE)
 }
 
